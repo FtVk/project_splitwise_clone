6e2318a6a9712fc732eb652eaa6b4581837e6dea
Armitta-with-2t <Armita.Hoda@gmail.com>
2024-12-27 21:53:23 +0330

    Update api.js
    deals with balance graph

--- splitwise-web/src/services/api.js
+++ splitwise-web/src/services/api.js
@@ -8,18 +8,23 @@ const api = {
   fetchGroups: () =>
     axios.get(`${API_BASE_URL}/groups`).then((res) => res.data.groups),
 
-
   addGroup: (groupName) =>
     axios.post(`${API_BASE_URL}/groups`, { name: groupName }).then((res) => res.data),
-
 
   fetchGroupMembers: (groupName) =>
     axios.get(`${API_BASE_URL}/groups/${groupName}/members`).then((res) => res.data.members),
 
-
   addMemberToGroup: (groupName, memberName) =>
     axios.post(`${API_BASE_URL}/groups/${groupName}/members`, { name: memberName }).then((res) => res.data),
 
+  fetchBalanceData: (groupName) =>
+    axios.get(`${API_BASE_URL}/groups/${groupName}/balance`).then((res) => res.data),  
+
+  simplifyDebts: (groupName) =>
+    axios.post(`${API_BASE_URL}/groups/${groupName}/simplify-debts`).then((res) => res.data),
+
+  saveBalanceData: (groupName, balanceData) =>
+    axios.post(`${API_BASE_URL}/groups/${groupName}/balance`, balanceData).then((res) => res.data),
 
   addTransaction: (groupName, transactionData) =>
     axios.post(`${API_BASE_URL}/groups/${groupName}/transactions`, transactionData).then((res) => res.data),
