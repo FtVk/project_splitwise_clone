e8a2ff8178f8229db73259d63809dda9a96de50f
ZSeyfollahi <zahrraseyfollahi@gmail.com>
2025-01-01 18:56:02 +0330

    Update graph.py

--- core/graph.py
+++ core/graph.py
@@ -2,14 +2,15 @@ from collections import defaultdict
 from datetime import datetime
 from core.balance_calculation import BalanceGraph
 from core.debt_simplification import DebtSimplification
+import uuid
 
 class ExpenseGraph:
     def __init__(self):
         # Graph to store detailed transaction history
-        self.graph = defaultdict(list)  # Stores detailed transactions
-        self.balance_graph = BalanceGraph()    # Underlying balance graph
+        self.graph = defaultdict(list)
+        self.balance_graph = BalanceGraph()
 
-    def add_transaction(self, from_user, to_user, amount, category, timestamp=None, explanation=None):
+    def add_transaction(self, from_user, to_user, amount, category, timestamp=None, explanation=None, transaction_id=None):
         """Add a transaction between users and update the balance graph."""
         if amount <= 0:
             raise ValueError("Amount must be positive.")
@@ -17,12 +18,16 @@ class ExpenseGraph:
         if timestamp is None:
             timestamp = datetime.utcnow().isoformat()
         
+        if transaction_id is None:
+            transaction_id = str(uuid.uuid4())
+
         transaction = {
             "to": to_user,
             "amount": amount,
             "category": category,
             "timestamp": timestamp,
-            "explanation": explanation
+            "explanation": explanation,
+            "transaction_id": transaction_id
         }
         
         # Add the transaction to the history
@@ -30,15 +35,26 @@ class ExpenseGraph:
 
         # Update the balance graph
         self.balance_graph.add_edge(from_user, to_user, amount)
-        
+    
+    def delete_transaction(self, transaction_id):
+        """Delete a specific transaction by its unique ID and update the balance graph."""
+        for user, transactions in self.graph.items():
+            for transaction in transactions:
+                if transaction["transaction_id"] == transaction_id:
+                    transactions.remove(transaction)
+                    self.balance_graph.add_edge(user, transaction["to_user"], -transaction["amount"])
+                    self.simplify_balances()
+
+                    return True
+        return False
+
+
     def fetch_recent_transactions(self):
         # Assuming selected_group is a list of users in the group
         all_transactions = self.get_all_transactions()
 
-        # Sort transactions by timestamp in descending order
         all_transactions.sort(key=lambda x: x["timestamp"], reverse=True)
 
-        # Return the last three transactions
         return all_transactions[:3]
 
     def visualize_transactions(self):
